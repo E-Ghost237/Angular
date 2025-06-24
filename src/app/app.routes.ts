722d4239d269df9121c3b5ae2b528096cc039555
Zangue Van <zanguevan31@gmail.com>
2025-06-24 10:27:25 +0100

    new route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { PicSnapList } from './pic-snap-list/pic-snap-list';
 import { LandingPage } from './landing-page/landing-page';
+import { SinglePicSnap } from './single-pic-snap/single-pic-snap';
 
 export const routes: Routes = [
+    {path: 'picsnap/:id', component: SinglePicSnap},
     { path: 'picsnap', component: PicSnapList },
-    {path: '', component: LandingPage}
+    {path: '', component: LandingPage} 
 ];
