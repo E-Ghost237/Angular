46a0527272863768b41b3f39a2c782b4c3bb79c1
Zangue Van <zanguevan31@gmail.com>
2025-06-24 10:25:49 +0100

    copy of pic snap for single page

---
+++ src/app/single-pic-snap/single-pic-snap.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { PicShare } from '../models/pic-share';
+import { NgClass, NgStyle, UpperCasePipe, DatePipe } from '@angular/common';
+import { PicSnapsService } from '../services/pic-snaps.service';
+
+@Component({
+  selector: 'app-sinle-pic-snap',
+  imports: [NgStyle, NgClass, UpperCasePipe, DatePipe],
+  templateUrl: './single-pic-snap.html',
+  styleUrl: './single-pic-snap.scss',
+})
+export class SinglePicSnap implements OnInit {
+  @Input() picshare!: PicShare;
+  
+  snapButtonText!: string;
+  userhasLiked!: boolean;
+
+ constructor(private PicSnapsService: PicSnapsService) { }
+
+  ngOnInit(): void {
+    this.snapButtonText = '👍';
+    this.userhasLiked = false;
+  }
+
+  onSnapImage(): void {
+    if (this.userhasLiked) {
+      this.dislike();
+    } else {
+      this.like();
+    }
+  }
+
+  like(): void {
+    this.PicSnapsService.likePicSnapById(this.picshare.id, 'like');
+    this.snapButtonText = '👎';
+    this.userhasLiked = true;
+  }
+
+  dislike(): void {
+    this.PicSnapsService.likePicSnapById(this.picshare.id, 'dislike');
+    this.snapButtonText = '👍';
+    this.userhasLiked = false;
+  }
+}
+
